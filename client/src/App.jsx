9b9da81764381ecb663dc7e01566739a73ae10f8
Oladipopreserve-19 <98063668+Oladipopreserve-19@users.noreply.github.com>
2024-04-13 12:39:54 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
 import Search from './pages/Search';
 
+.
 export default function App() {
   return (
     <BrowserRouter>
